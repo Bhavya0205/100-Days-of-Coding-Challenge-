f9061fb220be85c4fed61bdc6e9298f1c13f3181
Bhavya0205 <bhavya.guglani02@gmail.com>
2023-10-06 15:13:56 +0530

    Counting Bits

---
+++ Code94.java
@@ -0,0 +1,44 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+
+0 <= n <= 105
+ */
+public class Code94 {
+    public static int[] countBits(int n) {
+        int[] res=new int[n+1];
+        for(int i=0;i<=n;i++){
+            res[i]=Integer.bitCount(i);
+        }
+        return res;
+    }
+
+    public static void main(String args[]){
+        int n=2;
+        System.out.println(countBits(n));
+    }
+}
